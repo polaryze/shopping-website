5c4467362964cc045864516cd4a5fb6453758114
Fateen <fateen.naqash@gmail.com>
2022-01-22 00:04:41 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const darkmode = document.getElementById('dark_mode').src;
+
+
+const menuOpen = document.querySelector(".menu");
+const menuClose = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
